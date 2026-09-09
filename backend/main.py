6cd271df3b30f833b3fdfeaa2898c3a8ada01a68
Akash Goswami <goswamiakash2006@gmail.com>
2026-09-09 20:36:42 +0530

    Allow production frontend

--- backend/main.py
+++ backend/main.py
@@ -43,7 +43,7 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://127.0.0.1:5173",],
+    allow_origins=["http://localhost:5173","http://127.0.0.1:5173","https://medikiosk-mo9d.onrender.com",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
